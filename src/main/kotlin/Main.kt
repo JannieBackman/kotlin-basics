119c55105c9681119434edadec9d3a8329d9b457
Jannie87 <jannie.backman@gmail.com>
2022-10-21 14:14:15 +0200

    functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main () {
-    val x = isEven(2)
-    println(x)
+    //val x = isEven(2)
+    val y = 3
+    println(y.isOdd() )
 
+}
+fun Int.isOdd() : Boolean {
+ return this % 2 == 1
 }
 fun isEven(number: Int) : Boolean {
     return number % 2 == 0
